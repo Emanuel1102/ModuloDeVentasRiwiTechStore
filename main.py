32b672631cbd5719aedb2fad28c5579b99950c1d
Emanuel Manotas <emanuelmanotas2002@gmail.com>
2026-03-06 15:48:12 -0500

    integré la función de capitalización

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-client_name=input('Nombre del cliente (sin espacios, ni numeros ni caracteres especiales) => ')
+from src.helpers.capitalize import text_capitalize
+
+client_name=input('Nombre del cliente (por favor sin numeros o caracteres especiales) => ')
 
 if client_name.isalpha():
     try:
@@ -8,15 +10,16 @@ if client_name.isalpha():
 
         for current_product in range(products_quantity):
             product_name=input(f'Ingresa el producto #{current_product+1} (nombre del producto) => ')
-            unitary_price=int(input(f'Precio unitario de "{product_name}" => $'))
-            product_quantity=int(input(f'Cantidad de "{product_name}" => '))
+            product_name_capitalize=text_capitalize(product_name)
+            unitary_price=int(input(f'Precio unitario de "{product_name_capitalize}" => $'))
+            product_quantity=int(input(f'Cantidad de "{product_name_capitalize}" => '))
             total_product=unitary_price*product_quantity
 
 
-            if product_name.isalpha():
+            if product_name_capitalize.isalpha():
                 if unitary_price and product_quantity:
                     new_product={
-                        'product_name':product_name,
+                        'product_name':product_name_capitalize,
                         'unitary_price': unitary_price,
                         'product_quantity':product_quantity,
                         'product_total': total_product
@@ -49,7 +52,7 @@ if client_name.isalpha():
 
         # Interfaz de los datos de la venta
         print(f'| {'-'*60} |')
-        print(f'| Cliente: {client_name}')
+        print(f'| Cliente: {text_capitalize(client_name)}')
         print(f'| {'_'*60} |')
         print(f'''| Product... | cant... |  Precio unit... |     total    |''')
         print(f'| {'-'*60} |')
